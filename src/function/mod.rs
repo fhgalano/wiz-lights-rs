650e7e375b640885af39e66df964050ce3aa5859
FideL <fhgalano@gmail.com>
2024-04-28 18:11:36 -0500

    created function module. Using traits as functions so that they can be individually implemented for devices on the server (i.e. bulbs or groups). Currently, just On/Off traits

---
+++ src/function/mod.rs
@@ -0,0 +1,43 @@
+// Defining all funcs to run on registry members
+use std::fmt;
+use std::error::Error;
+use std::fmt::Formatter;
+
+use crate::bulb::response::ErrorResponse;
+
+
+#[derive(Debug, Clone)]
+pub struct FunctionError {
+    function_name: String,
+    inner_error: String,
+}
+
+impl FunctionError {
+    pub fn new(function_name: String, inner_error: String) -> FunctionError {
+        FunctionError {
+            function_name,
+            inner_error,
+        }
+    }
+}
+
+impl fmt::Display for FunctionError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "An error occurred during WizFunction {} - {}",
+            self.function_name,
+            self.inner_error,
+        )
+    }
+}
+
+impl Error for FunctionError {}
+
+pub trait On {
+    fn on(&self) -> Result<bool, ErrorResponse>;
+}
+
+pub trait Off {
+    fn off(&self) -> Result<bool, ErrorResponse>;
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod bulb;
 
 mod utils;
+mod function;
