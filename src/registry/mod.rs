c4e20fdf279d6d106a15eb95155c1bcb75abe62a
FideL <fhgalano@gmail.com>
2024-04-28 02:02:43 -0500

    changed functions to accept any surrealdb type

--- src/registry/mod.rs
+++ src/registry/mod.rs
@@ -4,8 +4,6 @@ mod surreal;
 
 use serde::{Serialize, Deserialize};
 use surrealdb::Surreal;
-use surrealdb::engine::local::Db;
-use surrealdb::sql::Thing;
 use surrealdb::engine::any::Any;
 
 use crate::bulb::Bulb;
@@ -18,7 +16,42 @@ struct Out {
 }
 
 
-async fn get_bulbs(db: &Surreal<Db>) -> surrealdb::Result<Vec<Bulb>> {
+    fn turn_off_by_id(&self, id: Id) -> Result<bool, FunctionError> {
+        for i in self.bulbs.iter().clone() {
+            if Id::from(i._id.clone() as i32) == id {
+                return i.off().map_err(|e| {
+                    FunctionError::new(
+                        "Off".to_string(),
+                        e.to_string(),
+                    )
+                })
+            };
+        }
+
+        for i in self.groups.iter() {
+            if i._id == id {
+                return i.on().map_err(|e| {
+                    FunctionError::new(
+                        "Off".to_string(),
+                        e.to_string(),
+                    )
+                })
+            }
+        }
+
+        Err(FunctionError::new(
+            "turn_off_by_id".to_string(),
+            MissingElementError {
+                _id: id,
+            }.to_string(),
+        ))
+    }
+    // fn add_bulb() -> surrealdb::Result<()> {};
+    // fn add_group() -> surrealdb::Result<()> {};
+    // fn detect_unknown_bulbs_on_network() -> Option<Vec<Ipv4Addr>> {};
+}
+
+
 async fn get_bulbs_from_db(db: &Surreal<Any>) -> surrealdb::Result<Vec<Bulb>> {
     let b: Vec<Bulb> = db.select("bulb").await?;
     dbg!(b.clone());
